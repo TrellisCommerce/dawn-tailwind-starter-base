8c207235b06908cc100efdce88ea7a87d3038550
renebreslauer <rene@absoluteweb.com>
2024-11-08 08:58:09 -0500

    fix build

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,53 @@
 import { defineConfig } from 'vite';
+import adastra from 'adastra-plugin';
+import path from 'path';
+import basicSsl from '@vitejs/plugin-basic-ssl';
 
-export default defineConfig({
-  root: '.', // Set the root to the project folder
-  build: {
-    outDir: 'dist', // This can be your output directory for production build
-    rollupOptions: {
-      input: 'src/entrypoints/index.js', // Set the entry point to your application
+const PACKAGE_PREFIX = 'wan';
+
+export default ({ command, mode }) => {
+  return defineConfig({
+    server: {
+      port: 3000,
+      // https: true,
     },
-  },
-  css: {
-    postcss: {
-      plugins: [require('tailwindcss'), require('autoprefixer')],
+    build: {
+      emptyOutDir: false,
+      rollupOptions: {
+        output: {
+          assetFileNames: `${PACKAGE_PREFIX}-[name]-[hash][extname]`,
+          entryFileNames: `${PACKAGE_PREFIX}-[name]-[hash].js`,
+          chunkFileNames: `${PACKAGE_PREFIX}-chunk-[name]-[hash].js`,
+        },
+      },
     },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
+      },
+    },
+
+    app: 'mpa',
+    plugins: [
+      adastra(),
+      delayFullReload(),
+      basicSsl({
+        /** name of certification */
+        name: 'model-motorcars',
+      }),
+    ],
+  });
+};
+
+const delayFullReload = () => ({
+  name: 'delay-full-reload',
+  handleHotUpdate({ server }) {
+    setTimeout(() => {
+      server.ws.send({
+        type: 'full-reload',
+      });
+    }, 1000);
+
+    return [];
   },
 });
